a4fd6e3d167df44b7d59d4fb80bd5c7c75813212
rachelmorgan22 <168668593+rachelmorgan22@users.noreply.github.com>
2024-11-07 12:47:35 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import { BrowserRouter basename="/Tourism-Website" as Router, Route, Routes } from 'react-router-dom';
-import Header from './components/Header';
 import Home from './components/Home';
 import Tours from './components/Tours';
 import FAQ from './components/FAQ';
 import './styles/App.css';
-import Footer from './components/Footer';
 
 const App = () => {
   return (
     <Router>
+    <Home />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/tours" element={<Tours />} />
