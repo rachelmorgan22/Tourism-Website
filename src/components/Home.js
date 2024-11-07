dfafd5c7240e8611100b89cc1c56aaa44e9260d3
rachelmorgan22 <168668593+rachelmorgan22@users.noreply.github.com>
2024-11-07 12:36:36 -0500

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,15 +2,19 @@ import React from 'react';
 import tourism from '../assets/tourism.jpg';
 import '../styles/Home.css';
 import TourCards from './TourCards';
+import Header from './Header';
+import Footer from './Footer';
 
 const Home = () => {
   return ( 
   <div>
+    <Header />
   <div className="hero">
   <img src={tourism} alt="Landscape" />
   <h3>Discover the Enchantment of Costa Rica!</h3>
   </div>
   <TourCards />
+  <Footer />
   </div>
   );
 };
